e8acc56098ebde8edf35f125f20c226cee047804
Taras Mazurkevych <mad.taras@gmail.com>
2017-04-21 10:22:37 +0300

    Refactoring

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -8,10 +8,11 @@ public class FastCollinearPoints {
     private ArrayList<LineSegment> lineSegments;
 
     public FastCollinearPoints(Point[] points) {
-        int i, j, k;
-
+        int i, j, k, m;
+        ArrayList<Set<Point>> sp = new ArrayList<>();
         ArrayList<Point[]> lineSegmentsPoints = new ArrayList<>();
         lineSegments = new ArrayList<LineSegment>();
+        boolean isDuplicate;
 
         if (points == null) {
             throw new java.lang.NullPointerException();
@@ -34,30 +35,40 @@ public class FastCollinearPoints {
 
         for (i = 0; i < points.length; i++) {
             Arrays.sort(pointsCopy, points[i].slopeOrder());
-            for (j = 0; j < pointsCopy.length - 2; j++) {
-                double slope1 = points[i].slopeTo(pointsCopy[j]);
-                double slope2 = points[i].slopeTo(pointsCopy[j+1]);
-                double slope3 = points[i].slopeTo(pointsCopy[j+2]);
 
-                if (slope1 == slope2 && slope2 == slope3) {
-                    selectedFour[0] = points[i];
-                    selectedFour[1] = pointsCopy[j];
-                    selectedFour[2] = pointsCopy[j+1];
-                    selectedFour[3] = pointsCopy[j+2];
+            int counter = 0;
+            Point[] tmpPoint = new Point[]{points[i], points[i]};
 
-                    sortArray(selectedFour);
+            for (j = 1; j < pointsCopy.length - 1; j++) {
+                if (points[i].slopeTo(pointsCopy[j]) == points[i].slopeTo(pointsCopy[j+1])) {
+                    counter++;
+                    if (tmpPoint[1].compareTo(pointsCopy[j]) < 0) {
+                        tmpPoint[1] = pointsCopy[j];
+                    } else if (tmpPoint[0].compareTo(pointsCopy[j]) > 0) {
+                        tmpPoint[0] = pointsCopy[j];
+                    }
+                    if (tmpPoint[1].compareTo(pointsCopy[j + 1]) < 0) {
+                        tmpPoint[1] = pointsCopy[j + 1];
+                    } else if (tmpPoint[0].compareTo(pointsCopy[j + 1]) > 0) {
+                        tmpPoint[0] = pointsCopy[j + 1];
+                    }
+                } else {
+                    if (counter >= 2) {
+                        isDuplicate = false;
+                        for (m = 0; m < lineSegmentsPoints.size(); m++) {
 
-                    boolean isDuplicate = false;
-                    for (int m = 0; m < lineSegmentsPoints.size(); m++) {
-
-                        if (lineSegmentsPoints.get(m)[0].compareTo(selectedFour[0]) == 0 &&
-                                lineSegmentsPoints.get(m)[1].compareTo(selectedFour[3]) == 0) {
-                            isDuplicate = true;
+                            if (lineSegmentsPoints.get(m)[0].compareTo(tmpPoint[0]) == 0 &&
+                                    lineSegmentsPoints.get(m)[1].compareTo(tmpPoint[1]) == 0) {
+                                isDuplicate = true;
+                            }
+                        }
+                        if (!isDuplicate) {
+                            lineSegmentsPoints.add(new Point[]{tmpPoint[0], tmpPoint[1]});
                         }
                     }
-                    if (!isDuplicate) {
-                        lineSegmentsPoints.add(new Point[]{selectedFour[0], selectedFour[3]});
-                    }
+
+                    counter = 0;
+                    tmpPoint = new Point[]{points[i], points[i]};
                 }
             }
         }
